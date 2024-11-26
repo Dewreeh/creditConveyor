b82d0d5d8af95fb68021f5c2f2a0dc9ffb1ebf94
dewreeh <severussenap@gmail.com>
2024-11-26 19:17:21 +0300

    added LoanStatementRequestDto

---
+++ Calculator/src/main/java/org/example/dto/LoanStatementRequestDto.java
@@ -0,0 +1,17 @@
+package org.example.dto;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class LoanStatementRequestDto {
+    private BigDecimal amount;
+    private Integer term;
+    private String firstName;
+    private String lastName;
+    private String middleName;
+    private String email;
+    private LocalDate birthdate;
+    private String passportSeries;
+    private String passportNumber;
+
+}
