1967299a4c39d22059f858d10a173f7b2a1e4085
dewreeh <severussenap@gmail.com>
2024-12-15 16:03:20 +0300

    entities defined

--- Deal/src/main/java/org/deal/Main.java
+++ Deal/src/main/java/org/deal/Main.java
@@ -3,8 +3,6 @@ package org.deal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
 
 @SpringBootApplication
 public class Main {
