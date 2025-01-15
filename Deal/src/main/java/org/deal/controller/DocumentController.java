f69a54276eb947e5c25995d9b69f730b8c30089d
dewreeh <severussenap@gmail.com>
2025-01-15 13:20:01 +0300

    kafka consumer stuff added and bugfixes in Deal

--- Deal/src/main/java/org/deal/controller/DocumentController.java
+++ Deal/src/main/java/org/deal/controller/DocumentController.java
@@ -27,7 +27,7 @@ public class DocumentController {
     }
 
     @PostMapping("{statementId}/send")
-    public ResponseEntity<Object> send(@PathVariable UUID statementId){
+    public ResponseEntity<Object> send(@PathVariable("statementId") UUID statementId){
         //Не было сказано про логику формирования документов, потому этот и последующие хэндлеры
         //по сути просто заглушки для имитации логики работы системы
 
@@ -47,13 +47,22 @@ public class DocumentController {
     }
 
     @PostMapping("{statementId}/sign")
-    public ResponseEntity<Object> sign(@PathVariable UUID statementId){
+    public ResponseEntity<Object> sign(@PathVariable("statementId") UUID statementId){
+        Statement statement = statementRepository.getByStatementId(statementId);
 
+        String email = statement.getClient().getEmail();
+
+        kafkaProducerService.sendMessage("credit-issued", new EmailMessageDto(
+                email,
+                Theme.SEND_SES,
+                statementId,
+                "Кредит выдан"
+        ));
         return ResponseEntity.ok().body("Документ подписан");
     }
 
     @PostMapping("{statementId}/code")
-    public ResponseEntity<Object> code(@PathVariable UUID statementId){
+    public ResponseEntity<Object> code(@PathVariable("statementId") UUID statementId){
         Statement statement = statementRepository.getByStatementId(statementId);
 
         String email = statement.getClient().getEmail();
