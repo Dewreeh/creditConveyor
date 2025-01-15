c122e70bf328da3051a956a09953653ca5de7444
dewreeh <severussenap@gmail.com>
2025-01-15 13:42:17 +0300

    logs and docs added

--- Deal/src/main/java/org/deal/controller/DocumentController.java
+++ Deal/src/main/java/org/deal/controller/DocumentController.java
@@ -1,5 +1,9 @@
 package org.deal.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.deal.dto.EmailMessageDto;
 import org.deal.enums.Theme;
 import org.deal.model.Statement;
@@ -13,6 +17,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.UUID;
 
+@Slf4j
 @Controller
 @RequestMapping("deal/document")
 public class DocumentController {
@@ -25,7 +30,17 @@ public class DocumentController {
         this.kafkaProducerService = kafkaProducerService;
         this.statementRepository = statementRepository;
     }
-
+    @Operation(
+            summary = "Запрос на отправку документов брокеру",
+            description = "Отправляет брокеру запрос на отправку документов для МС dossier.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Формируются документы",
+                            content = @Content(mediaType = "application/json")
+                    )
+            }
+    )
     @PostMapping("{statementId}/send")
     public ResponseEntity<Object> send(@PathVariable("statementId") UUID statementId){
         //Не было сказано про логику формирования документов, потому этот и последующие хэндлеры
@@ -34,8 +49,9 @@ public class DocumentController {
         //по заявке получаем почту клиента
         Statement statement = statementRepository.getByStatementId(statementId);
 
+        log.info("Получен запрос на отправку документов по заявке {}", statementId);
         String email = statement.getClient().getEmail();
-
+        log.info("Получена почта клиента по заявке {}: ", statementId);
         kafkaProducerService.sendMessage("send-documents", new EmailMessageDto(
                 email,
                 Theme.SEND_DOCUMENTS,
@@ -43,9 +59,21 @@ public class DocumentController {
                 "Ваш ПЭП-код ****"
         ));
 
+
         return ResponseEntity.ok().body("Формируются документы");
     }
 
+    @Operation(
+            summary = "Запрос на подпись документов брокеру",
+            description = "Отправляет брокеру запрос на подпись документов для МС dossier.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Документ подписан",
+                            content = @Content(mediaType = "application/json")
+                    )
+            }
+    )
     @PostMapping("{statementId}/sign")
     public ResponseEntity<Object> sign(@PathVariable("statementId") UUID statementId){
         Statement statement = statementRepository.getByStatementId(statementId);
@@ -58,9 +86,21 @@ public class DocumentController {
                 statementId,
                 "Кредит выдан"
         ));
+        log.info("Получен запрос на подписание документов по заявке {}", statementId);
         return ResponseEntity.ok().body("Документ подписан");
     }
 
+    @Operation(
+            summary = "Запрос на отправку кода ПЭП брокеру",
+            description = "Отправляет брокеру запрос на отправку кода ПЭП для МС dossier.",
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "Код ПЭП",
+                            content = @Content(mediaType = "application/json")
+                    )
+            }
+    )
     @PostMapping("{statementId}/code")
     public ResponseEntity<Object> code(@PathVariable("statementId") UUID statementId){
         Statement statement = statementRepository.getByStatementId(statementId);
@@ -73,7 +113,7 @@ public class DocumentController {
                 statementId,
                 "Ваш ПЭП-код ****"
         ));
-
+        log.info("Получен запрос на отправку кода ПЭП по заявке {}", statementId);
         return ResponseEntity.ok().body("Запрос отправлен");
     }
 }
--- Deal/src/main/java/org/deal/service/KafkaProducerService.java
+++ Deal/src/main/java/org/deal/service/KafkaProducerService.java
@@ -1,9 +1,11 @@
 package org.deal.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.deal.dto.EmailMessageDto;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class KafkaProducerService {
 
@@ -16,7 +18,7 @@ public class KafkaProducerService {
     }
 
     public void sendMessage(String topic, EmailMessageDto message) {
-
+        log.info("Отправка сообщения брокеру {}, {}, {}", message.getAdress(), message.getStatementId(), message.getText());
         kafkaTemplate.send(topic, message);
     }
 }
